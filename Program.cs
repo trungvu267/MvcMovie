7bf7761071ebe9efa84fe70829b73b4a2337eb1e
TrungVu <vutrung26072001@gmail.com>
2022-12-24 17:15:39 +0700

    add model

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Data;
 var builder = WebApplication.CreateBuilder(args);
 
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<MvcMovieContext>(options =>
+        options.UseSqlite(builder.Configuration.GetConnectionString("MvcMovieContext")));
+}
+else
+{
+    builder.Services.AddDbContext<MvcMovieContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ProductionMvcMovieContext")));
+}
 // Add services to the container.
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
